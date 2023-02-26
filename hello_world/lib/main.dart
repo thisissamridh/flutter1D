7f4d9eb5c19b3fadf2e128a4dede13c8dfc4bc02
thisissamridh <samridhsinghind@gmail.com>
2023-02-26 18:20:48 +0530

    added an image

--- hello_world/lib/main.dart
+++ hello_world/lib/main.dart
@@ -5,9 +5,21 @@ void main() {
     MaterialApp(
       // if we use const here, we will get an error because MaterialApp is not a const constructor
       home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueAccent.shade100,
+          title: const Text('I am a title'),
+        ),
         backgroundColor: Colors.greenAccent.shade200, // here colors is a enum
-        body: const Center(
-          child: Text('Hello World!'),
+        body: Center(
+          child: Column(
+            children: const <Widget>[
+              Text('Hello World!'),
+              Image(
+                  image: NetworkImage(
+                'https://images.alphacoders.com/164/164903.jpg',
+              )),
+            ],
+          ),
         ),
       ),
     ),
